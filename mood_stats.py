7095cc721ea25a57aad646bc2d1c8052e5038510
Elizaveta <elizaveta.ermakovich@tuni.fi>
2023-08-18 20:15:14 +0300

    add coefficients into a dictionary

--- mood_stats.py
+++ mood_stats.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import date, timedelta
 
 
 input_file = "mood_tracker_stats\mood_tracker.csv"
@@ -15,3 +16,28 @@ for row in input_content:
     output_content.writerow(clean_row)
 
 finput.close()
+
+coefficients = {'vo':1, 
+                'su': 0.95,
+                'ho': 0.85,
+                'ra': 0.8,
+                'gl': 0.75,
+                'sp': 0.7,
+                'no': 0.6,
+                'st': 0.5,
+                'gr': 0.4,
+                'mi': 0.35,
+                'tr': 0.25,
+                'pl': 0.2,
+                'ag': 0.2,
+                'ty': 0.15,
+                'st': 0.1,
+                'po': 0.05,
+                'ot': 0}
+
+
+def compound_formula():
+    today = date.today()
+    day_of_year = today.timetuple().tm_yday
+
+
