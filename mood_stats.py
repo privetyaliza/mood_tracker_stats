b5d0d8474d1be1cdbd9e914fe64f81b19fb81e34
Elizaveta <elizaveta.ermakovich@tuni.fi>
2023-08-18 21:13:47 +0300

    add calculating sum

--- mood_stats.py
+++ mood_stats.py
@@ -15,29 +15,50 @@ for row in input_content:
     clean_row = row[:14]
     output_content.writerow(clean_row)
 
+# output_file = csv.writer(output_content[:-1])
 finput.close()
+foutput.close()
 
-coefficients = {'vo':1, 
-                'su': 0.95,
-                'ho': 0.85,
-                'ra': 0.8,
-                'gl': 0.75,
-                'sp': 0.7,
-                'no': 0.6,
-                'st': 0.5,
-                'gr': 0.4,
-                'mi': 0.35,
-                'tr': 0.25,
-                'pl': 0.2,
-                'ag': 0.2,
-                'ty': 0.15,
-                'st': 0.1,
-                'po': 0.05,
-                'ot': 0}
+coefficients = {'восхитительно': 1, 
+                'супер': 0.95,
+                'хорошо': 0.85,
+                'глубоко': 0.75,
+                'работоспособно': 0.8,
+                'спокойно': 0.7,
+                'нормально': 0.6,
+                'мимо': 0.35,
+                'странно': 0.5,
+                'тревожно': 0.25,
+                'грустно': 0.4,
+                'тяжело': 0.15,
+                'плохо': 0.2,
+                'стрессово': 0.1,
+                'по-больному': 0.05,
+                'агрессивно': 0.2,
+                'отвратительно': 0}
 
 
 def compound_formula():
     today = date.today()
     day_of_year = today.timetuple().tm_yday
 
+    # moods = []
 
+    foutput = open(output_file, 'r', encoding='utf-8')
+    data = csv.reader(foutput)
+
+    sum_of_moods = 0
+
+    next(data)
+    for row_num, row in enumerate(data, start=1):
+        if row_num <= 17:
+
+            sum_of_moods += int(row[1]) * coefficients[row[0]]
+            print(sum_of_moods)
+        else:
+            break
+
+
+
+compound_formula()
+print(coefficients['агрессивно'])
